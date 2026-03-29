cf7a8e6b50c9f799b9703b49ff23a7399b6943c2
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:48:33 -0300

    Update login_viewmodel.dart

--- lib/app/viewmodels/login_viewmodel.dart
+++ lib/app/viewmodels/login_viewmodel.dart
@@ -1,1 +1,10 @@
+import '../data/usuario_mock_store.dart';
 
+class LoginViewModel {
+  final _store = UsuarioMockStore();
+
+  bool autenticar(String login, String senha) {
+    final usuario = _store.buscar(login, senha);
+    return usuario != null;
+  }
+}
