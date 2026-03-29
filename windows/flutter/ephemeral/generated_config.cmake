3c39c9a43f9071efecfc53d52a0ff0bd20cde0d6
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 17:27:23 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova"
+  "FLUTTER_TARGET=C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OTA2NzNhNGVlZg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NmMwYmFhZWJmNw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\icaro\\OneDrive\\Área de Trabalho\\aualas marcelo\\Prova\\Prova\\.dart_tool\\package_config.json"
+)
