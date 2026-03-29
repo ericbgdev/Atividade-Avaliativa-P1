52d97229824bd0006512bb86933dc629907e26a6
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:48:12 -0300

    Implement UsuarioMockStore with singleton pattern

--- lib/app/data/usuario_mock_store.dart
+++ lib/app/data/usuario_mock_store.dart
@@ -1,1 +1,23 @@
+import '../models/usuario_model.dart';
 
+class UsuarioMockStore {
+  // Lista em memória exigida [5, 7]
+  final List<UsuarioModel> _usuarios = [];
+
+  // Singleton: instância única para todo o app [6]
+  static final UsuarioMockStore _instance = UsuarioMockStore._internal();
+  factory UsuarioMockStore() => _instance;
+  UsuarioMockStore._internal();
+
+  void adicionar(UsuarioModel usuario) => _usuarios.add(usuario);
+
+  UsuarioModel? buscar(String login, String senha) {
+    try {
+      return _usuarios.firstWhere(
+        (u) => u.login == login && u.senha == senha
+      );
+    } catch (e) {
+      return null;
+    }
+  }
+}
