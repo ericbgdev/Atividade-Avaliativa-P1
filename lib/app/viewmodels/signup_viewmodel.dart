42ab32215b4c5eea4ea2deb1a1278236d918acc1
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:48:51 -0300

    Implement signup functionality in SignupViewModel

--- lib/app/viewmodels/signup_viewmodel.dart
+++ lib/app/viewmodels/signup_viewmodel.dart
@@ -1,1 +1,11 @@
+import '../data/usuario_mock_store.dart';
+import '../models/usuario_model.dart';
 
+class SignupViewModel {
+  final _store = UsuarioMockStore();
+
+  void cadastrar(String nome, String login, String senha) {
+    final novo = UsuarioModel(nome: nome, login: login, senha: senha);
+    _store.adicionar(novo);
+  }
+}
