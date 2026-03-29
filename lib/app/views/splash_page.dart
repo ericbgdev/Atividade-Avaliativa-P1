1086db99e7bddf5dfd8be27661a78c758de14ed0
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:49:20 -0300

    Add SplashPage with navigation to login
    Implement a splash screen that navigates to the login page after 3 seconds.

--- lib/app/views/splash_page.dart
+++ lib/app/views/splash_page.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Timer de alguns segundos exigido [2]
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container( // Requisito: Widgets básicos [2]
+        width: double.infinity,
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.school, size: 80, color: Colors.white),
+            SizedBox(height: 20),
+            Text("Projeto ADS", style: TextStyle(color: Colors.white, fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
