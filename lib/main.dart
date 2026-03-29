0ac49d7cdcfc7c29b75265a84b26b23ecdf547f3
Ícaro <icaro.godoy@sou.unifeob.edu.br>
2026-03-29 16:17:46 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'app/views/splash_page.dart';
-import 'app/views/login_page.dart';
-import 'app/views/signup_page.dart';
-import 'app/views/home_page.dart';
 
-void main() => runApp(const MyApp());
+import 'app/app_widget.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/', // Requisito: Splash é a primeira [2]
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/login': (context) => const LoginPage(),
-        '/cadastro': (context) => const SignupPage(),
-        '/home': (context) => const HomePage(),
-      },
-    );
-  }
+void main() {
+  runApp(AppWidget());
 }
