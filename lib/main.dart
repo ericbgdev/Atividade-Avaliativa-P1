1d714eec813dcc4d0537ccc45af4bf070d524883
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:50:37 -0300

    Set up Flutter app with initial routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'app/views/splash_page.dart';
+import 'app/views/login_page.dart';
+import 'app/views/signup_page.dart';
+import 'app/views/home_page.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/', // Requisito: Splash é a primeira [2]
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/login': (context) => const LoginPage(),
+        '/cadastro': (context) => const SignupPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
