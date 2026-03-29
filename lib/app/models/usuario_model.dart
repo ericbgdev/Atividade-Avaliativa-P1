4d11d6efcd0f0e83e6b1f5901857523ff5c9c801
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:47:47 -0300

    Add UsuarioModel class with required fields

--- lib/app/models/usuario_model.dart
+++ lib/app/models/usuario_model.dart
@@ -1,1 +1,11 @@
+class UsuarioModel {
+  final String nome;
+  final String login; // Pode ser e-mail
+  final String senha;
 
+  UsuarioModel({
+    required this.nome,
+    required this.login,
+    required this.senha,
+  });
+}
