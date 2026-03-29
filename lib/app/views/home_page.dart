06ddc97d6968ce18b8bbfdce630d3637d56a38a5
Eric <eric.gudera@sou.unifeob.edu.br>
2026-03-29 14:50:19 -0300

    Create HomePage widget with icon and welcome text
    Added a HomePage widget with a centered icon and text.

--- lib/app/views/home_page.dart
+++ lib/app/views/home_page.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: Center( // Requisito: Ícone e texto centralizado [6]
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.home, size: 100, color: Colors.blue),
+            Text("Bem-vindo à Home", style: TextStyle(fontSize: 22)),
+          ],
+        ),
+      ),
+    );
+  }
+}
